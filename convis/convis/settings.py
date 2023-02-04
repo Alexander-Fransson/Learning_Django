35d7985eb045cf75df6ba6a1a382da6f1af89a0c
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2023-02-04 10:16:09 +0100

    creating first migrations to create database tables

--- convis/convis/settings.py
+++ convis/convis/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', #<- all apps app.py Config classes have to be added
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
