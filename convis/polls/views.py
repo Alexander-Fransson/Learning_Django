b6bc324df82ee9f9c863c36dda019e2f738c83d1
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2023-02-04 19:37:31 +0100

    creating templates

--- convis/polls/views.py
+++ convis/polls/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render, get_object_or_404
+from .models import Question
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("Hellow world, this is the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def details(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+    results = "You're looking at the results of question %s." 
+    return HttpResponse(results % question_id)
+
+def vote(request, question_id):
+    results = "You are voteing on question %s."
+    return HttpResponse(results % question_id)
