95c8cea9624d996a13950c3a74237d372093cc6e
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2023-02-05 11:08:06 +0100

    testing with django

--- convis/polls/views.py
+++ convis/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -10,11 +11,20 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5] #<- meanes before five and becouse it starts from zero its the first five returned
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now() #<- _lte filters out things that are less than or equal to the value (propably)
+        ).order_by('-pub_date')[:5] #<- meanes before five and becouse it starts from zero its the first five returned
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
